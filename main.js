907f0b3e2d66ec649e75829cbc48c9418a2249f6
leonwi03 <92833364+leonwi03@users.noreply.github.com>
2022-02-07 19:14:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+let rTop = document.querySelector(".fa-chevron-up");
+
+
+rTop.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+
+    });
+};
+
+
+
+
+
+let clikLink = document.querySelector("#link");
+let menu = document.querySelector("#menu");
+
+clikLink.addEventListener("click", readMore);
+
+function readMore() {
+    if (clikLink.className == "link") {
+        menu.style.display = "block";
+        clikLink.className = "link hidden";
+    } else if (clikLink.className = "link hidden") {
+        menu.style.display = "none";
+        clikLink.className = "link";
+    }
+}
+
+
+let face = document.getElementById("fb");
+let twitter = document.getElementById("tw");
+let telegram = document.getElementById("te");
+let snap = document.getElementById("sn");
+
+face.addEventListener("click", function () {
+    window.open("https://www.facebook.com/", "_blank")
+})
+twitter.addEventListener("click", function () {
+    window.open("https://twitter.com/home", "_blank", "width=768, height= 1280, top=200, left= 350")
+})
+
+
+
+function change(x) {
+    x.classList.toggle("change");
+}
